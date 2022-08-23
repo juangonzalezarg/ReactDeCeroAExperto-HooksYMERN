3d35e96971837849f6c25a1b0c3b2f999e80abd2
Juan Manuel González <juangonzalez664@gmail.com>
2022-08-23 17:56:38 -0300

    Commit 20220823

--- 08-journal-app/src/auth/pages/LoginPage.jsx
+++ 08-journal-app/src/auth/pages/LoginPage.jsx
@@ -1,23 +1,11 @@
 import { Link as RouterLink } from 'react-router-dom';
 import { Grid, Typography, TextField, Button, Link } from '@mui/material';
 import { Google } from '@mui/icons-material';
+import { AuthLayout } from '../layout/AuthLayout';
 
 export const LoginPage = () => {
   return (
-    <Grid
-      container
-      spacing={ 0 }
-      direction="column"
-      alignContent="center"
-      justifyContent="center"
-      sx={{ minHeight: '100vh', backgroundColor: 'primary.main', padding: 4 }}
-    >
-      <Grid
-        className="box-shadow"
-        xs={ 3 }
-        sx={{ backgroundColor: 'white', padding: 3, borderRadius: 2 }}>
-          
-          <Typography variant='h5' sx={{ mb: 1 }}>Login</Typography>
+    <AuthLayout title="Login" >
 
           <form>
             <Grid container>
@@ -61,7 +49,6 @@ export const LoginPage = () => {
             </Grid>
           </form>
 
-      </Grid>
-    </Grid>
+      </AuthLayout>
   )
 }
