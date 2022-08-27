4b00a7136d0937e397f9ee393be657b321aea298
Juan Manuel González <juangonzalez664@gmail.com>
2022-08-27 12:03:22 -0300

    Clase 20220827

---
+++ 08-journal-app/src/journal/views/NothingSelectedView.jsx
@@ -0,0 +1,24 @@
+import { Grid, Typography } from '@mui/material';
+import { StarOutline } from '@mui/icons-material';
+
+export const NothingSelectedView = () => {
+  return (
+    <Grid
+        container
+        spacing={ 0 }
+        direction="column"
+        alignContent="center"
+        alignItems="center"
+        justifyContent="center"
+        sx={{ minHeight: 'calc(100vh - 110px)', backgroundColor: 'primary.main', borderRadius: 3 }}
+    >       
+        <Grid >
+            <StarOutline sx={{ fontSize: 100, color: 'white' }} />
+        </Grid>
+
+        <Grid >
+            <Typography color='white' variant='h5'>Selecciona o crea una entrada</Typography>
+        </Grid>
+    </Grid>
+  )
+}
