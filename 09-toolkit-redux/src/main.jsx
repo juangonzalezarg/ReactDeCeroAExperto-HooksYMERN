897d88ce2fb7db53c33936f7a31be3450beb7697
Juan Manuel González <juangonzalez664@gmail.com>
2022-08-28 19:29:46 -0300

    Clase 20220828

--- 09-toolkit-redux/src/main.jsx
+++ 09-toolkit-redux/src/main.jsx
@@ -5,12 +5,13 @@ import { Provider } from 'react-redux';
 
 import { store } from './store';
 // import App from './App';
-import { PokemonApp } from './PokemonApp';
+// import { PokemonApp } from './PokemonApp';
+import { TodoApp } from './TodoApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <PokemonApp />
+      <TodoApp />
     </Provider>
   </React.StrictMode>
 )
