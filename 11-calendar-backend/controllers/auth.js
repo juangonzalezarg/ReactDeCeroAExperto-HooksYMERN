09078a7c067f9cfaf81c4b60e62968b04d49e41f
Juan Manuel González <juangonzalez664@gmail.com>
2022-09-24 11:08:42 -0300

    Clase 202209241108

---
+++ 11-calendar-backend/controllers/auth.js
@@ -0,0 +1,57 @@
+const { response } = require('express');
+const Usuario = require('../models/Usuario');
+
+const crearUsuario = async( req, res = response ) => {
+    
+    // const { name, email, password } = req.body;
+
+   try {
+       
+        const usuario = new Usuario( req.body );
+   
+        await usuario.save();
+
+        res.status(201).json({
+            ok: true,
+            msg: 'register',
+        });
+    
+   } catch (error) {
+
+        console.log(error)
+        res.status(500).json({
+            ok: false,
+            msg: 'Por favor comuniquese con el administrador del sistema',
+        });
+        
+   }
+
+}
+
+const loginUsuario = ( req, res = response ) => {
+    
+    const { email, password } = req.body;
+
+    res.status(201).json({
+        ok: true,
+        msg: 'login',
+        email,
+        password
+    });
+
+}
+
+const revalidarToken = ( req, res = response ) => {
+    
+    res.json({
+        ok: true,
+        msg: 'renew'
+    });
+
+}
+
+module.exports = {
+    crearUsuario,
+    loginUsuario,
+    revalidarToken
+}
---
+++ 11-calendar-backend/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+require('dotenv').config();
+const { dbConnection } = require('./database/config');
+
+// Crear el servidor de express
+const app = express();
+
+// Base de datos
+dbConnection();
+
+// Directorio público
+app.use( express.static('public') );
+
+// Lectura y parseo del body
+app.use( express.json() );
+
+// Rutas
+// TODO: auth // crear, login, renew
+app.use('/api/auth', require('./routes/auth'));
+// TODO: CRUD: Eventos
+
+
+
+
+
+// Escuchar peticiones
+app.listen( process.env.PORT, () => {
+    console.log(`Servidor corriendo en puerto ${ process.env.PORT }`);
+})
