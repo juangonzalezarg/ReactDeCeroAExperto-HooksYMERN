2a9cb1863f1c2994a793ca08c7a3e5d53cc302bb
Juan Manuel González <juangonzalez664@gmail.com>
2022-09-27 18:02:17 -0300

    Clase 202209271802

---
+++ 11-calendar-backend/controllers/events.js
@@ -0,0 +1,66 @@
+const { response } = require('express');
+const Evento = require('../models/Eventos');
+
+const getEventos = async( req, res = response ) => {
+
+    const eventos = await Evento.find()
+                                .populate('user', 'name');
+                                //.populate('user', 'name email');
+    res.status(200).json({
+        ok: true,
+        eventos
+    });
+
+}
+
+const crearEvento = async( req, res = response ) => {
+    
+    const evento = new Evento( req.body );
+
+    try {
+
+        evento.user = req.uid;
+        
+        const eventoGuardado = await evento.save();
+
+        res.status(200).json({
+            ok: true,
+            evento: eventoGuardado
+        });
+
+    } catch (error) {
+
+        console.log(error)
+        res.status(500).json({
+            ok: false,
+            msg: 'Hable con el administrador del sistema'
+        });
+
+    }
+
+}
+
+const actualizarEvento = ( req, res = response ) => {
+    
+    res.status(200).json({
+        ok: true,
+        msg: 'actualizarEvento'
+    });
+
+}
+
+const eliminarEvento = ( req, res = response ) => {
+    
+    res.status(200).json({
+        ok: true,
+        msg: 'eliminarEvento'
+    });
+
+}
+
+module.exports = {
+    getEventos,
+    crearEvento,
+    actualizarEvento,
+    eliminarEvento
+}
